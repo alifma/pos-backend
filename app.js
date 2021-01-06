a56e98f3e5be40fe430163da04641508894880cf
alifma <alifmaulana26@gmail.com>
2021-01-06 07:45:52 +0700

    Display DB 1 file

--- app.js
+++ app.js
@@ -4,6 +4,41 @@ const app = express()
 // Atur Port
 const port = 3000
 
+app.get('/', (req, res) => {
+    const json = [{
+            id: 10,
+            name: 'Alifma'
+        },
+        {
+            id: 11,
+            name: 'Maulana'
+        }
+    ]
+    res.json(json)
+})
+
+// Koneksi ke Mysql
+const mysql = require('mysql2')
+
+// Koneksi DB
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'posvue'
+})
+
+// Display all Menus
+app.get('/menus', (req, res) => {
+    connection.query('SELECT * FROM t_menu', (error, result) => {
+        if (error) {
+            res.send(error)
+        } else {
+            res.json(result)
+        }
+    })
+})
+
 
 app.listen(port, () => {
     console.log(`Application is Started on localhost:${port}`)
