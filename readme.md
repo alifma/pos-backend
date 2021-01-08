f2c940221f1a62aaafdcccc9340e4268547c5e41
alifma <alifmaulana26@gmail.com>
2021-01-08 18:10:19 +0700

    edit readme

--- readme.md
+++ readme.md
@@ -1,2 +1,11 @@
 # Backend POS
 Backend untuk aplikasi Point of Sales
+## Dependencies
+Packages Installed
+- Body-Parser
+- Cors
+- Dotenv
+- Express
+- mysql2
+## Deployment
+This Application not deployed yet
--- res/controllers/orders.js
+++ res/controllers/orders.js
@@ -13,105 +13,151 @@ module.exports = {
         const page = req.query.page == undefined ? '1' : req.query.page
         const limit = 3
         const offset = page === 1 ? 0 : (page - 1) * limit
-        const sort = req.query.sort == undefined ? 'ASC' : req.query.sort
-        modelAllOrders(offset, limit, sort)
-            .then((response) => {
-                if (response.length != 0) {
-                    res.json(response)
-                } else {
-                    res.json({
-                        message: "No Data on this page",
-                        status: "ERROR"
-                    })
-                }
+        const sort = req.query.sort == undefined ? 'asc' : (req.query.sort).toLowerCase()
+        const availSort = ['asc', 'desc']
+        if (isNaN(page) || availSort.includes(sort.toLowerCase()) == false) {
+            res.json({
+                message: "Wrong parameter",
+                status: "ERROR"
             })
-            .catch((error) => {
-                res.send(error.message)
-            })
+        } else {
+            modelAllOrders(offset, limit, sort)
+                .then((response) => {
+                    if (response.length != 0) {
+                        res.json(response)
+                    } else {
+                        res.json({
+                            message: "No Data on this page",
+                            status: "ERROR"
+                        })
+                    }
+                })
+                .catch((error) => {
+                    res.send(error.message)
+                })
+        }
     },
     getDetailOrders: (req, res) => {
         const inv = req.params.inv
-        modelDetailOrders(inv)
-            .then((response) => {
-                if (response.length != 0) {
-                    res.json(response)
-                } else {
-                    res.json({
-                        message: 'Data Not Found!',
-                        status: 'ERROR'
-                    })
-                }
+        if (isNaN(inv)) {
+            res.json({
+                message: "Wrong Invoice Type",
+                status: "ERROR"
             })
-            .catch((error) => {
-                res.send(error.message)
-            })
+        } else {
+            modelDetailOrders(inv)
+                .then((response) => {
+                    if (response.length != 0) {
+                        res.json(response)
+                    } else {
+                        res.json({
+                            message: 'Data Not Found!',
+                            status: 'ERROR'
+                        })
+                    }
+                })
+                .catch((error) => {
+                    res.send(error.message)
+                })
+        }
     },
     deleteOrders: (req, res) => {
         const inv = req.params.inv
-        modelDeleteOrders(inv)
-            .then((response) => {
-                if (response.affectedRows != 0) {
-                    res.json({
-                        message: 'Orders Deleted!',
-                        status: 'OK'
-                    })
-                } else {
-                    res.json({
-                        message: 'Nothing Deleted!',
-                        status: 'ERROR'
-                    })
-                }
+        if (isNaN(inv)) {
+            res.json({
+                message: "Wrong Invoice Type",
+                status: "ERROR"
             })
-            .catch((error) => {
-                res.send(error.message)
-            })
+        } else {
+            modelDeleteOrders(inv)
+                .then((response) => {
+                    if (response.affectedRows != 0) {
+                        res.json({
+                            message: 'Orders Deleted!',
+                            status: 'OK'
+                        })
+                    } else {
+                        res.json({
+                            message: 'Nothing Deleted, no match!',
+                            status: 'ERROR'
+                        })
+                    }
+                })
+                .catch((error) => {
+                    res.send(error.message)
+                })
+        }
     },
     postOrders: (req, res) => {
         const data = req.body
-        modelPostOrders(data)
-            .then(() => {
-                res.json({
-                    message: 'Orders Added!',
-                    status: 'OK'
+        if (data.length != 0) {
+            modelPostOrders(data)
+                .then(() => {
+                    res.json({
+                        message: 'Orders Added!',
+                        status: 'OK'
+                    })
                 })
-            })
-            .catch((error) => {
-                res.send(error.message)
-            })
+                .catch((error) => {
+                    res.send(error.message)
+                })
+        } else {
+            res.send("ERROR : Please fill all field!")
+        }
     },
     deleteOrdersDtl: (req, res) => {
         const id = req.query.id
-        modelDeleteDetails(id)
-            .then((response) => {
-                if (response.affectedRows != 0) {
-                    res.json({
-                        message: 'Item Deleted from Order!',
-                        status: 'OK'
-                    })
-                } else {
-                    res.json({
-                        message: 'Nothing Deleted!',
-                        status: 'ERROR'
-                    })
-                }
+        if (isNaN(id)) {
+            res.json({
+                message: "Wrong ID Type",
+                status: "ERROR"
             })
-            .catch((error) => {
-                res.send(error)
-            })
+        } else {
+            modelDeleteDetails(id)
+                .then((response) => {
+                    if (response.affectedRows != 0) {
+                        res.json({
+                            message: 'Item Deleted from Order!',
+                            status: 'OK'
+                        })
+                    } else {
+                        res.json({
+                            message: 'Nothing Deleted!',
+                            status: 'ERROR'
+                        })
+                    }
+                })
+                .catch((error) => {
+                    res.send(error)
+                })
+        }
     },
     updateOrdersDtl: (req, res) => {
-        const data = req.body
         const id = req.params.id
-        modelUpdateDetails(data, id)
-            .then(() => {
-                res.json({
-                    message: 'Item Updated!',
-                    status: 'OK'
+        const currDate = new Date().toISOString().slice(0, 19).replace('T', ' ');
+        const data = {
+            ...req.body,
+            "updated_at": currDate
+        }
+        if (isNaN(id)) {
+            res.json({
+                message: "Wrong ID Type",
+                status: "ERROR"
+            })
+        } else if (data.length == 0) {
+            res.send("ERROR : Please fill all field!")
+        } else {
+            modelUpdateDetails(data, id)
+                .then(() => {
+                    res.json({
+                        message: 'Item Updated!',
+                        status: 'OK'
+                    })
                 })
-            })
-            .catch((error) => {
-                res.send(error)
-            })
+                .catch((error) => {
+                    res.send(error)
+                })
+        }
     }
 
 }
