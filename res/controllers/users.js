d91f490da61536bd94985824873f6b8767ef6d3b
root <root@ip-172-31-49-120.ec2.internal>
2021-02-03 22:43:15 +0700

    Save server

--- res/controllers/users.js
+++ res/controllers/users.js
@@ -48,6 +48,7 @@ module.exports = {
         .catch((err)=> error(res, 400, 'Registration Failed', err.message))
       }
     })
-    error(res, 400, 'Registration Failed', 'Please fill all data')
+    .catch((err)=> error(res, 400, 'Registration Failed', err.message))
+   // error(res, 400, 'Registration Failed', 'Please fill all data')
   },
 }
